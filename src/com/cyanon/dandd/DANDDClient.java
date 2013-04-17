c28c37905e12e26a2cea2be58753c6ca414db124
Cyborgs Anonymous <cyborgsanonymous@googlemail.com>
2013-04-17 22:59:19 +0100

    /message command implemented

--- src/com/cyanon/dandd/DANDDClient.java
+++ src/com/cyanon/dandd/DANDDClient.java
@@ -5,12 +5,15 @@ package com.cyanon.dandd;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 import com.cyanon.dandd.attacktype.Attack;
 import com.cyanon.dandd.battle.Battle;
 import com.cyanon.dandd.battle.Lobby;
 import com.cyanon.dandd.networking.ClientInfoPacket;
+import com.cyanon.dandd.networking.Packet;
 import com.cyanon.dandd.networking.ServerInfoPacket;
+import com.cyanon.dandd.networking.StringPacket;
 
 public class DANDDClient extends Thread {
 	
@@ -22,6 +25,8 @@ public class DANDDClient extends Thread {
 	
 	protected Attack nextAttack;
 	protected String thisPlayerHandle;
+	
+	private Boolean clientLive = false;
 		
 	public DANDDClient(Socket s, String thisGameName, Lobby lobby)
 	{
@@ -45,17 +50,46 @@ public class DANDDClient extends Thread {
 			System.out.println("Boo");
 			e.printStackTrace();
 		}
+		clientLive = true;
 		System.out.println(thisPlayerHandle + " has joined the lobby with " + thisLobby.addNewBattle(this) + " other people..."); //Fix for person/people
 	}
 
 	public void run()
 	{
-
+		while(clientLive)
+		{
+			Packet packet;
+			try 
+			{
+				packet = (Packet)ois.readObject();
+				processPacket(packet);
+			} 
+			catch (ClassNotFoundException e) 
+			{
+				e.printStackTrace();
+			}
+			catch (SocketException reset)
+			{
+				System.out.println(thisPlayerHandle + " has left the game.");
+				clientLive = false;
+			} catch (IOException e) {
+				System.err.println("Dead!");
+			}
+			
+		}
 	}
 
 	public void refreshClientLobby() throws IOException
 	{
 		oos.writeObject(thisLobby);
 		oos.flush();
+	}
+	
+	private void processPacket(Packet packet)
+	{
+		if (packet instanceof StringPacket)
+		{
+			System.out.println(thisPlayerHandle + " said:" + packet.getPayload());
+		}
 	}
 }
